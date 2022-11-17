20623fa2a088f95094a790f40c1cde1aa0ee2177
Milton D. Yama <miltondyama@outlook.com>
2022-11-17 00:42:33 -0400

    criei a migration para remover a descricao da pergunta

--- Migrations/20221117040745_RemoverDescricaoDaPergunta.Designer.cs
+++ Migrations/20221117040745_RemoverDescricaoDaPergunta.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VistoriaDeVeiculos.DataContext;
 
 namespace VistoriaDeVeiculos.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20221117040745_RemoverDescricaoDaPergunta")]
+    partial class RemoverDescricaoDaPergunta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20221117040745_RemoverDescricaoDaPergunta.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VistoriaDeVeiculos.Migrations
+{
+    public partial class RemoverDescricaoDaPergunta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Pergunta");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Pergunta",
+                type: "varchar(300)",
+                nullable: true);
+        }
+    }
+}
